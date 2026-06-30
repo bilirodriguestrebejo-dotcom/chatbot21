611fcd6643a12520980bbcad82e0ea49298ad6ba
bilirodriguestrebejo-dotcom <bilirodriguestrebejo@gmail.com>
2026-06-29 18:48:53 -0600

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,6 @@
 import sys
 from pathlib import Path
 
-
 # Agregar la carpeta backend al path de Python
 BACKEND_DIR = Path(__file__).resolve().parent / "chatbot 5" / "backend"
 sys.path.insert(0, str(BACKEND_DIR))
